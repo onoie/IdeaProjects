23846fd50299f49535d8ce3fb62bf1f623342f0d
onoie <onoie3@gmail.com>
2018-02-16 11:42:10 +0900

    fast commit

--- scala/HelloWorld/src/Main.scala
+++ scala/HelloWorld/src/Main.scala
@@ -1,15 +1,41 @@
+import scala.compat.Platform.EOL
+
 object HelloWorld {
   def main(args: Array[String]){
+    println("Hello,World!")
     if(args.size == 0) {
-      val strs = Array("Hello", "World")
-      output(strs)
+      val strs = Array("Hello"+EOL+"NewWorld")
+      puts(strs)
+      var capital = Map("US" -> "Washington")
+      capital += ("Japan" -> "Tokyo")
+      println(capital("Japan")) //Tokyo
     } else {
       printf("Invalid args.")
     }
   }
-  def output(strs: Array[String]):Unit = {
+  var str = "string"
+  def getStr: String = str
+  def setStr(str: String):Unit ={
+    this.str=str
+  }
+  val f: Function1[Int, String] = new Function1[Int, String] {
+    def apply(arg: Int): String = arg.toString
+  }
+  f.apply(10)
+  def concatAsString(a: Int, b: Int): String = {
+    val a_ = a.toString
+    val b_ = b.toString
+    a_.+(b_)
+  }
+  def puts(strs: Array[String]):Unit = {
     for(str <- strs) {
       printf("%s ", str)
     }
+  }
+  def oneline(str: String):Unit = {
+    printf("%s ",str.replaceAll("(\r\n)|\r|\n", ""))
+  }
+  def pre(str: String): Unit ={
+    printf("%s ",str.replaceAll("(\r\n)|\r|\n", EOL))//sys.props("line.separator")
   }
 }
